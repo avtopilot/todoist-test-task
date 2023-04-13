d0abe6c72ad306d014bec2c5a25440ff6bdfac92
Julia Tolipova <julia.tolipova@gmail.com>
2023-04-13 17:38:27 +0200

    Add integration tests

--- server/tests/Todoist.WebApi.IntegrationTests/TaskControllerShould.cs
+++ server/tests/Todoist.WebApi.IntegrationTests/TaskControllerShould.cs
@@ -1,7 +1,7 @@
 using System.Net;
+using FluentAssertions;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using FluentAssertions;
 using RestSharp;
 using Todoist.WebApi.IntegrationTests.Fixtures;
 using Xunit;
@@ -15,9 +15,107 @@ public class TaskControllerShould : TestBase
     }
 
     [Fact]
+    public async Task ReturnErrorOnCreatingTaskWithInvalidStatus()
+    {
+        // arrange
+        var request = new RestRequest("v1/task/test")
+            .AddJsonBody(new { status = 555, priority = 1});
+
+        // act
+        var response = await RestClient.ExecutePostAsync(request);
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task ReturnErrorOnCreatingTaskWithInvalidPriority()
+    {
+        // arrange
+        var request = new RestRequest("v1/task/test")
+            .AddJsonBody(new { status = 0, priority = 555});
+
+        // act
+        var response = await RestClient.ExecutePostAsync(request);
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Theory]
+    [InlineData("test")]
+    [InlineData("test space")]
+    [InlineData("test@ #5")]
+    public async Task ReturnSuccessOnCreatingValidTask(string taskName)
+    {
+        // arrange
+        var request = new RestRequest($"v1/task/{taskName}")
+            .AddJsonBody(new { status = 0, priority = 0});
+
+        // act
+        var response = await RestClient.ExecutePostAsync(request);
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        // remove the record
+        var requestToRemove = new RestRequest($"v1/task/{taskName}");
+        var responseForRemoval = await RestClient.DeleteAsync(requestToRemove);
+
+        // validate successfully removed
+        responseForRemoval.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task ReturnErrorOnCreatingDuplicatedTask()
+    {
+        // arrange
+        const string TaskName = "test";
+        var request = new RestRequest($"v1/task/{TaskName}")
+            .AddJsonBody(new { status = 0, priority = 0});
+
+        // act
+        // first addition of a task
+        var response = await RestClient.ExecutePostAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        // second addition of a task
+        var responseDuplicate = await RestClient.ExecutePostAsync(request);
+        responseDuplicate.StatusCode.Should().Be(HttpStatusCode.Conflict);
+
+        // remove the record
+        var requestToRemove = new RestRequest($"v1/task/{TaskName}");
+        var responseForRemoval = await RestClient.DeleteAsync(requestToRemove);
+
+        // validate successfully removed
+        responseForRemoval.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task ReturnErrorOnRemovingUnExistingTask()
+    {
+        // arrange
+        const string TaskName = "do not exist";
+        var request = new RestRequest($"v1/task/{TaskName}");
+
+        // act
+        var response = await RestClient.DeleteAsync(request);
+
+        // arrange
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
     public async Task ReturnListOfTasks()
     {
         // arrange
+        // add one task
+        const string TaskName = "test";
+        var requestToAdd = new RestRequest($"v1/task/{TaskName}")
+            .AddJsonBody(new { status = 0, priority = 0});
+        await RestClient.ExecutePostAsync(requestToAdd);
+
+        // build request to list API endpoint
         var request = new RestRequest("v1/tasks");
 
         // act
@@ -25,5 +123,48 @@ public class TaskControllerShould : TestBase
 
         // assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+        Assert.NotNull(response.Content);
+
+        // remove added task
+        var requestToRemove = new RestRequest($"v1/task/{TaskName}");
+        var responseForRemoval = await RestClient.DeleteAsync(requestToRemove);
+
+        // validate successfully removed
+        responseForRemoval.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Theory]
+    [InlineData("test")]
+    [InlineData("test space")]
+    public async Task ReturnTaskOnRequest(string taskName)
+    {
+        // arrange
+        var expected = JObject.FromObject(
+            new { name = taskName, priority = 0, status = 0 });
+
+        // add one task
+        var requestToAdd = new RestRequest($"v1/task/{taskName}")
+            .AddJsonBody(new { status = 0, priority = 0});
+        await RestClient.ExecutePostAsync(requestToAdd);
+
+        // build request to get task API endpoint
+        var request = new RestRequest($"v1/task/{taskName}");
+
+        // act
+        var response = await RestClient.ExecuteGetAsync(request);
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        Assert.NotNull(response.Content);
+
+        var task = JsonConvert.DeserializeObject<JObject>(response.Content);
+        Assert.Equal(expected, task);
+
+        // remove added task
+        var requestToRemove = new RestRequest($"v1/task/{taskName}");
+        var responseForRemoval = await RestClient.DeleteAsync(requestToRemove);
+
+        // validate successfully removed
+        responseForRemoval.StatusCode.Should().Be(HttpStatusCode.NoContent);
     }
 }
